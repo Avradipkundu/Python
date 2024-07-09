b8bc95ac85d66d3252cfdd1dae87ed9721ee83c9
Avradipkundu <avradipkundu851@.gmailcom>
2024-07-10 00:12:24 +0530

    update

--- college/findLen.py
+++ college/findLen.py
@@ -1,3 +1,15 @@
-my_list = [10, 20, 30, 40, 50]
-length = len(my_list)
-print(f"Length of the list using len(): {length}")
+list=[1,2,3,4,5,6,7,8,9]
+
+# Using len() Function
+length1=len(list)
+print("Length of the list: ",length1)
+
+# Using a Loop
+length=0
+for i in list:
+    length+=1
+print("Length of the list: ",length)
+
+# Using List Comprehension
+length2=sum([1 for _ in list])
+print("Length of the list: ",length2)
